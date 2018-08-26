d8023fad5a577e500b96197395f4b6386eae3b6e
Justin Revelstoke <theartofpour@gmail.com>
2018-08-26 12:02:53 -0700

    combined query example

--- simple/stats.py
+++ simple/stats.py
@@ -26,6 +26,19 @@ class Stats:
             group by styleID
             having count(styleID) > 7000
             order by ct desc;
+            
+        eg. combined
+            select h.id, h.name, count(rh.recipeID) as ct
+            from recipe_hops rh
+            join hops h on rh.hopID = h.id
+            join recipes r on rh.recipeID = r.id
+            join recipe_fermentables rf on r.id = rf.recipeID
+            join fermentables f on rf.fermentableID = f.id
+            where r.styleID = 4 -- american ipa
+            and f.id = 27 -- golden promise
+            group by rh.hopID
+            --having count(styleID) > 2000
+            order by ct desc;
         '''
 
         return []
